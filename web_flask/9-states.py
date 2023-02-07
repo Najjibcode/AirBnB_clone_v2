408c5d653029e9bda97b88cbf15d834925d849cd
Najjibcode <jibzaj@gmail.com>
2023-02-07 08:20:28 +0300

    webflsk

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,55 @@
 #!/usr/bin/python3
-
-"""Display all states or one state if it has id"""
+""" Script starts a Flask Web Application
+listening on 0.0.0.0, port 5000
+"""
 
 from flask import Flask, render_template
-from models import storage
+import models
 from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def all_states():
-    """Display all states"""
-    states = list(storage.all(State).values())
-    return render_template("9-states.html", states=states)
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """Displays HTML of all States in Stored Chronologically"""
+    states = models.storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
-@app.route("/states/<id>", strict_slashes=False)
-def one_state(id):
-    """Display one state"""
-    states = list(storage.all(State).values())
-    return render_template("9-states.html", states=states, id=id)
+@app.route('/cities_by_states', strict_slashes=False)
+def list_state_cities():
+    """Displays HTML of states and its cities"""
+    states = models.storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.route('/states', strict_slashes=False)
+def list_states():
+    """Displays HTML of states Chronologically"""
+    states = models.storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_id(id):
+    """Displays HTML of States based on id"""
+    flag = 0
+    states = None
+    all_states = models.storage.all(State).values()
+    for state in all_states:
+        if id in state.id:
+            flag = 1
+            states = state
+            break
+    return render_template('9-states.html', states=states, flag=flag)
 
 
 @app.teardown_appcontext
-def tear_down(exception):
-    """Ends Session"""
-    storage.close()
+def teardown(db):
+    """Closes or otherwise deallocates resource if it exists"""
+    models.storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host='0.0.0.0', port=5000)
